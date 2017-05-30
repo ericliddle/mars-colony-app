d29eb3b3561a32feebb852649ade2f509f09e38e
heyliddle <eric.liddle@gmail.com>
2017-05-30 09:31:09 -0700

    Initial Commit

---
+++ src/app/models/alien.ts
@@ -0,0 +1,18 @@
+export interface Alien {
+    "type": string;
+    "submitted_by": string;
+    "id": string;
+    "description": string;
+}
+
+// interface KatGreeting {
+//   verb: string;
+//   directObject: string;
+// }
+
+// function katgreeter(greeting: KatGreeting) {
+//   console.log('{greeting}`);
+// }
+
+
+// katgreeter({verb: '', directObject: ''});
---
+++ src/app/models/colonist.ts
@@ -0,0 +1,10 @@
+export class Colonist {
+    name: string;
+    age: string;
+    job_id: string;
+    constructor(name, age, jobID) {
+        this.name = name;
+        this.age = age;
+        this.job_id = jobID
+    }
+}
